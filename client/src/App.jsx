14eb391faaa3b7a5ed9a4fd6eed5930ca4492edb
A P Prajwal <apprajwalwork@gmail.com>
2025-03-23 11:53:50 +0530

    connection responses etc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
 
 export default function App() {
+  const socket = io("http://localhost:3000");
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected: ", socket.id);
+    });
+    socket.on("welcome", (msg) => {
+      console.log(msg);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return <div>App</div>;
 }
--- server/app.js
+++ server/app.js
@@ -1,13 +1,28 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("HII");
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket connected");
+  console.log(socket.id);
+  socket.on("disconnect", () => {
+    console.log("Disconnected", socket.id);
+  });
 });
 
 server.listen(3000, () => {
